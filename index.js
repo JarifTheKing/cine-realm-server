0516bfc930961160928a72f1e6d4fc18d7c312ef
JarifTheKing <jarifanuwar7.com>
2025-11-26 16:19:03 +0600

    hm

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
+// MongoDB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -20,7 +20,8 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function connectDB() {
+// WRAP DB CONNECT INSIDE ASYNC FUNCTION
+async function run() {
   try {
     await client.connect();
     console.log("MongoDB Connected Successfully!");
@@ -171,7 +172,7 @@ async function connectDB() {
   }
 }
 
-connectDB();
+run();
 
-// Vercel needs this export (NO app.listen)
+// EXPORT FOR VERCEL
 module.exports = app;
